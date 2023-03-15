81b8d061f0d1730fd574648556b07543a523e928
Manuel Burdach <manuelburdach@outlook.de>
2023-03-15 12:44:15 +0100

    Upload lev1_2

--- 15.03/lev1_2/function.js
+++ 15.03/lev1_2/function.js
@@ -10,7 +10,18 @@ export const lastOfArray = (array) => {
   return array[array.length - 1];
 };
 
-// export const = (array)=>{}
-// export const = (array)=>{}
-// export const = (array)=>{}
-// export const = (array)=>{}
+export const notFirstOfArray = (array) => {
+  return array.slice(1, array.length);
+};
+
+export const deleteItemOfArray = (array, item) => {
+  return array.filter((element) => element !== item);
+};
+
+export const uniqueItemsOfArray = (array) => {
+  return array.filter((x, i) => array.indexOf(x) === i);
+};
+
+export const sumOfArray = (array) => {
+  return array.reduce((a, b) => a + b, 0);
+};
--- 15.03/lev1_2/index.js
+++ 15.03/lev1_2/index.js
@@ -1,11 +1,30 @@
 import { names, numbers } from "./data.js";
-import { firstOfArray, notTheLastOfArray, lastOfArray } from "./function.js";
+import {
+  firstOfArray,
+  notTheLastOfArray,
+  lastOfArray,
+  notFirstOfArray,
+  deleteItemOfArray,
+  uniqueItemsOfArray,
+  sumOfArray,
+} from "./function.js";
 
-console.log(firstOfArray(names));
-console.log(firstOfArray(numbers));
+console.log("1. " + firstOfArray(names));
+console.log("1. " + firstOfArray(numbers));
 
-console.log(notTheLastOfArray(names));
-console.log(notTheLastOfArray(numbers));
+console.log("2. " + notTheLastOfArray(names));
+console.log("2. " + notTheLastOfArray(numbers));
 
-console.log(lastOfArray(names));
-console.log(lastOfArray(numbers));
+console.log("3. " + lastOfArray(names));
+console.log("3. " + lastOfArray(numbers));
+
+console.log("4. " + notFirstOfArray(names));
+console.log("4. " + notFirstOfArray(numbers));
+
+console.log("5. " + deleteItemOfArray(names, "Christian"));
+console.log("5. " + deleteItemOfArray(numbers, 1));
+
+console.log("6. " + uniqueItemsOfArray(names));
+console.log("6. " + uniqueItemsOfArray(numbers));
+
+console.log("6. " + sumOfArray(numbers));
